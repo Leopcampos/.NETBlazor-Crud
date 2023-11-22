597d5da77a1d3df5d280c936b4f2b00f80ed08f3
Leonardo Pereira Campos <leonardocampos@fiotec.fiocruz.br>
2023-11-22 18:04:52 -0300

    Criando uma biblioteca de classes para compartilhar as bibliotecas, criando model e repositories.

---
+++ src/CRUDWithBlazor/CRUDWithBlazor/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharedLibrary.Models;
+
+namespace CRUDWithBlazor.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions opts) : base(opts)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
