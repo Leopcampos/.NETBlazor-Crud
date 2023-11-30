6f0ccb0f52c00b79f82d5840d6c556aecd8325a2
Leonardo Pereira Campos <leonardocampos@fiotec.fiocruz.br>
2023-11-30 16:32:09 -0300

    Fazendo a vanegação para criar um novo produto.

--- src/SharedLibrary/Models/Product.cs
+++ src/SharedLibrary/Models/Product.cs
@@ -1,9 +1,15 @@
-﻿namespace SharedLibrary.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharedLibrary.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
         public int Quantity { get; set; }
     }
 }
